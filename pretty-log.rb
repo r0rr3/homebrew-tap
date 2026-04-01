2cac0968c96ee076240013c4e817abd7e3ede433
r0rr3 <wjsh1119@gmail.com>
2026-04-01 22:50:36 +0800

    Update SHA256 checksum for pretty-log.rb

--- pretty-log.rb
+++ pretty-log.rb
@@ -2,7 +2,7 @@ class PrettyLog < Formula
   desc "A fast log prettifier for JSON logs with streaming support"
   homepage "https://github.com/jsooo/pretty-log"
   url "https://github.com/jsooo/pretty-log/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
   depends_on "rust" => :build
